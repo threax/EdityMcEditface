167bac2d3d72d0e2757de74af26117458476ea89
Andrew Piper <threax@users.noreply.github.com>
2016-06-10 05:32:42 -0400

    Partial template list work.

--- EdityMcEditface/Example/edity/editor/js/new.js
+++ EdityMcEditface/Example/edity/editor/js/new.js
@@ -1,4 +1,25 @@
 ﻿(function ($, h)
 {
+    var templates = $('#TemplateList');
+    h.run(templates, function ()
+    {
+        return [
+            h.rest.get('/ide/api/dir/edity/templates/'),
+            h.func(function (data)
+            {
+                //This should be sanitized
+                var templateList = '';
+                data.data.files.forEach(function (i)
+                {
+                    templateList += '<li><a data-template="' + i + '">' + i + '</a></li>';
+                });
+                templates.html(templateList);
 
+                $('[data-template]').off('click.new').on('click.new', function ()
+                {
+
+                });
+            })
+        ]
+    });
 })(jQuery, htmlrest)

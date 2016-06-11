1caac95800aa9bd87858137daa39bd222bd3a86e
Andrew Piper <threax@users.noreply.github.com>
2016-06-11 15:37:39 -0400

    Seeing if we can port to .net core, basically an empty project now.

---
+++ EdityMcEditface.NetCore/wwwroot/_references.js
@@ -0,0 +1,4 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../gulpfile.js" />
+/// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
+/// <reference path="lib/jquery/dist/jquery.js" />

3e293791c136b58e635c6c999c657ab00aa49120
Andrew Piper <piper.andrew@spcollege.edu>
2016-06-30 12:32:13 -0400

    Added a populate fail message with a button to reload.

--- EdityMcEditface/wwwroot/edity/layouts/edit.js
+++ EdityMcEditface/wwwroot/edity/layouts/edit.js
@@ -123,10 +123,15 @@
     var settingsLifecycle = new h.form.ajaxLifecycle({
         formQuery: '#SettingsForm',
         loadingDisplayQuery: '#SettingsLoading',
-        mainDisplayQuery: '#SettingsForm'
+        mainDisplayQuery: '#SettingsForm',
+        populateFailDisplayQuery: '#SettingsLoadFailed'
     });
 
     $('#SettingsButton').click(function () {
+        settingsLifecycle.populateData();
+    });
+
+    $('#LoadSettingsAgainButton').click(function () {
         settingsLifecycle.populateData();
     });
 

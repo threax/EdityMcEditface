00815199006f2a1a74282d291b6593a1cc34f743
Andrew Piper <piper.andrew@spcollege.edu>
2016-07-07 15:04:27 -0400

    Customized ck editor styles.

--- EdityMcEditface/custom_components/ckeditor/styles.js
+++ EdityMcEditface/custom_components/ckeditor/styles.js
@@ -13,7 +13,7 @@
 // Otherwise it is strongly recommended to customize this file to match your
 // website requirements and design properly.
 
-CKEDITOR.stylesSet.add( 'default', [
+CKEDITOR.stylesSet.add('default', [
 	/* Block Styles */
 
 	// These styles are already available in the "Format" combo ("format" plugin),
@@ -30,18 +30,23 @@ CKEDITOR.stylesSet.add( 'default', [
 	{ name: 'Preformatted Text',element: 'pre' },
 	{ name: 'Address',			element: 'address' },
 	*/
-
-	{ name: 'Italic Title',		element: 'h2', styles: { 'font-style': 'italic' } },
-	{ name: 'Subtitle',			element: 'h3', styles: { 'color': '#aaa', 'font-style': 'italic' } },
-	{
-		name: 'Special Container',
-		element: 'div',
-		styles: {
-			padding: '5px 10px',
-			background: '#eee',
-			border: '1px solid #ccc'
-		}
-	},
+    { name: 'Alert Success', element: 'div', attributes: { 'class': 'alert alert-success', role: 'alert' } },
+    { name: 'Alert Info', element: 'div', attributes: { 'class': 'alert alert-info', role: 'alert' } },
+    { name: 'Alert Warning', element: 'div', attributes: { 'class': 'alert alert-warning', role: 'alert' } },
+    { name: 'Alert Danger', element: 'div', attributes: { 'class': 'alert alert-danger', role: 'alert' } },
+    { name: 'Panel', element: 'div', attributes: { 'class': 'panel' } },
+    { name: 'Well', element: 'div', attributes: { 'class': 'well' } },
+	//{ name: 'Italic Title',		element: 'h2', styles: { 'font-style': 'italic' } },
+	//{ name: 'Subtitle',			element: 'h3', styles: { 'color': '#aaa', 'font-style': 'italic' } },
+	//{
+	//	name: 'Special Container',
+	//	element: 'div',
+	//	styles: {
+	//		padding: '5px 10px',
+	//		background: '#eee',
+	//		border: '1px solid #ccc'
+	//	}
+	//},
 
 	/* Inline Styles */
 
@@ -57,55 +62,38 @@ CKEDITOR.stylesSet.add( 'default', [
 	{ name: 'Superscript',		element: 'sup' },
 	*/
 
-	{ name: 'Marker',			element: 'span', attributes: { 'class': 'marker' } },
+	//{ name: 'Marker',			element: 'span', attributes: { 'class': 'marker' } },
+    //
+	{ name: 'Big', element: 'big' },
+	{ name: 'Small', element: 'small' },
+	{ name: 'Typewriter', element: 'tt' },
 
-	{ name: 'Big',				element: 'big' },
-	{ name: 'Small',			element: 'small' },
-	{ name: 'Typewriter',		element: 'tt' },
+	{ name: 'Computer Code', element: 'code' },
+	{ name: 'Keyboard Phrase', element: 'kbd' },
+	{ name: 'Sample Text', element: 'samp' },
+	{ name: 'Variable', element: 'var' },
 
-	{ name: 'Computer Code',	element: 'code' },
-	{ name: 'Keyboard Phrase',	element: 'kbd' },
-	{ name: 'Sample Text',		element: 'samp' },
-	{ name: 'Variable',			element: 'var' },
+	{ name: 'Deleted Text', element: 'del' },
+	{ name: 'Inserted Text', element: 'ins' },
 
-	{ name: 'Deleted Text',		element: 'del' },
-	{ name: 'Inserted Text',	element: 'ins' },
-
-	{ name: 'Cited Work',		element: 'cite' },
-	{ name: 'Inline Quotation',	element: 'q' },
-
-	{ name: 'Language: RTL',	element: 'span', attributes: { 'dir': 'rtl' } },
-	{ name: 'Language: LTR',	element: 'span', attributes: { 'dir': 'ltr' } },
+	{ name: 'Cited Work', element: 'cite' },
+	{ name: 'Inline Quotation', element: 'q' },
+    //
+	//{ name: 'Language: RTL',	element: 'span', attributes: { 'dir': 'rtl' } },
+	//{ name: 'Language: LTR',	element: 'span', attributes: { 'dir': 'ltr' } },
 
 	/* Object Styles */
 
 	{
-		name: 'Styled image (left)',
-		element: 'img',
-		attributes: { 'class': 'left' }
+	    name: 'Styled image (left)',
+	    element: 'img',
+	    attributes: { 'class': 'pull-left' }
 	},
 
 	{
-		name: 'Styled image (right)',
-		element: 'img',
-		attributes: { 'class': 'right' }
-	},
-
-	{
-		name: 'Compact table',
-		element: 'table',
-		attributes: {
-			cellpadding: '5',
-			cellspacing: '0',
-			border: '1',
-			bordercolor: '#ccc'
-		},
-		styles: {
-			'border-collapse': 'collapse'
-		}
-	},
-
-	{ name: 'Borderless Table',		element: 'table',	styles: { 'border-style': 'hidden', 'background-color': '#E6E6FA' } },
-	{ name: 'Square Bulleted List',	element: 'ul',		styles: { 'list-style-type': 'square' } }
-] );
+	    name: 'Styled image (right)',
+	    element: 'img',
+	    attributes: { 'class': 'pull-right' }
+	}
+]);
 

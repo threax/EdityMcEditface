7d868be3c37ed819915095ed1f13b1b75020093b
Andrew Piper <piper.andrew@spcollege.edu>
2016-07-11 13:34:59 -0400

    Made auth controller redirect to given url.

--- EdityMcEditface/Controllers/AuthController.cs
+++ EdityMcEditface/Controllers/AuthController.cs
@@ -20,21 +20,43 @@ namespace EdityMcEditface.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> LogIn()
+        public async Task<IActionResult> LogIn(String returnUrl)
         {
             if (authChecker.IsValid)
             {
                 await HttpContext.Authentication.SignInAsync(authChecker.AuthenticationScheme, authChecker.ClaimsPrincipal);
             }
-            return Redirect("~/");
+
+            return SafeRedirect(ref returnUrl);
         }
 
         [HttpGet]
-        public async Task<IActionResult> LogOut()
+        public async Task<IActionResult> LogOut(String returnUrl)
         {
             await HttpContext.Authentication.SignOutAsync(authChecker.AuthenticationScheme);
 
-            return Redirect("~/");
+            return SafeRedirect(ref returnUrl);
+        }
+
+        private IActionResult SafeRedirect(ref string returnUrl)
+        {
+            //See if we have anything
+            if (String.IsNullOrEmpty(returnUrl))
+            {
+                returnUrl = "~/";
+            }
+
+            //Make sure they aren't trying to redirect to another website
+            try
+            {
+                Uri uri = new Uri(returnUrl, UriKind.Relative);
+            }
+            catch(UriFormatException)
+            {
+                returnUrl = "~/";
+            }
+
+            return Redirect(returnUrl);
         }
     }
 }

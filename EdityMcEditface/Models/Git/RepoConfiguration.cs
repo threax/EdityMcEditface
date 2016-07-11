82018682fb57d52d2411b11f1fc715d4f73bb442
Andrew Piper <piper.andrew@spcollege.edu>
2016-07-11 08:30:04 -0400

    Separated out app settings

---
+++ EdityMcEditface/Models/Git/RepoConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EdityMcEditface.Models.Git
+{
+    public class RepoConfiguration
+    {
+        private String projectDir;
+
+        public RepoConfiguration(String projectDir)
+        {
+            this.projectDir = projectDir;
+        }
+
+        public Repository getUserRepository(String userName)
+        {
+            var repo = Path.Combine(projectDir, "UserRepos", userName);
+            if (!Directory.Exists(repo))
+            {
+                Directory.CreateDirectory(repo);
+            }
+            return new Repository(repo);
+        }
+
+        public Repository getPublishedRepository()
+        {
+            var repo = Path.Combine(projectDir, "Published");
+            if (!Directory.Exists(repo))
+            {
+                Directory.CreateDirectory(repo);
+            }
+            return new Repository(repo);
+        }
+
+        public Repository getMasterRepository()
+        {
+            var repo = Path.Combine(projectDir, "Master");
+            if (!Directory.Exists(repo))
+            {
+                Directory.CreateDirectory(repo);
+            }
+            return new Repository(repo);
+        }
+    }
+}

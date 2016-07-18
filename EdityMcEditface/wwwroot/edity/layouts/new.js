727ff769323bc94e9eee8c7b3acf465caaac7807
Andrew Piper <piper.andrew@spcollege.edu>
2016-07-18 11:43:23 -0400

    Fixed new and started building pages for the manual.

--- EdityMcEditface/wwwroot/edity/layouts/new.js
+++ EdityMcEditface/wwwroot/edity/layouts/new.js
@@ -6,20 +6,21 @@
         //This should be sanitized, also check for server error
         //TODO: use a component
         h.component.repeat("new-template-preview", templates, data, function (element, data) {
-            element.bind('click.new', function ()
-            {
-                var sender = $(this);
-                h.rest.get(sender.attr('data-template') + ".html", function (templateData)
-                {
-                    //Make a blob
-                    var blob = new Blob([templateData], { type: "text/html" });
-                    h.rest.upload(templates.attr('data-template-list') + window.location.pathname + ".html", blob, function ()
-                    {
-                        window.location.href = window.location.href + ".html";
-                    });
-                });
-                
-                return false;
+            element.bind({
+                Create: {
+                    click: function (evt) {
+                        evt.preventDefault();
+
+                        var sender = $(this);
+                        h.rest.get(sender.attr('data-template') + ".html", function (templateData) {
+                            //Make a blob
+                            var blob = new Blob([templateData], { type: "text/html" });
+                            h.rest.upload(templates.attr('data-template-list') + window.location.pathname + ".html", blob, function () {
+                                window.location.href = window.location.href + ".html";
+                            });
+                        });
+                    }
+                }
             });
         });
     });

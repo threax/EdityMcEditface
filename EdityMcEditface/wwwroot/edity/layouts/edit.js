7c83833508148b0972e4a85d6c7f7e98446b2115
Andrew Piper <piper.andrew@spcollege.edu>
2016-07-21 08:05:56 -0400

    Removed dead way of finding buttons for the nav menu.

--- EdityMcEditface/wwwroot/edity/layouts/edit.js
+++ EdityMcEditface/wwwroot/edity/layouts/edit.js
@@ -4,12 +4,7 @@ jsns.run(function (using) {
     var storage = using("htmlrest.storage");
     var component = using("htmlrest.components");
 
-    var navMenu = Sizzle('[data-editor-navmenu]')[0];
-    var navItems = Sizzle('[data-editor-navitem]');
-    navItems.forEach(function (item) {
-        navMenu.appendChild(item);
-        item.style.display = "";
-    });
+    var navMenu = $('[data-editor-navmenu]')[0];
 
     var navItems = storage.getInInstance("edit-nav-menu-items", []);
     navItems.forEach(function (item) {

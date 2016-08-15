f856cdf183eea6ee397a1cdb2cf024210ac49442
Andrew Piper <piper.andrew@spcollege.edu>
2016-08-15 13:24:42 -0400

    Fixed menu items that are bound late from not showing up.

--- EdityMcEditface/wwwroot/edity/layouts/edit.js
+++ EdityMcEditface/wwwroot/edity/layouts/edit.js
@@ -22,22 +22,18 @@ jsns.define("edity.pageSourceSync", [
 });
 
 jsns.run([
-    "htmlrest.storage",
     "htmlrest.components",
     "htmlrest.domquery",
     "htmlrest.widgets.navmenu"
 ],
-function(exports, module, storage, component, domQuery, navMenuWidget) {
-
+function (exports, module, component, domQuery, navmenu) {
     var navMenu = domQuery.first('[data-editor-navmenu]');
 
-    var navItems = storage.getInInstance("edit-nav-menu-items", []);
-    navItems.forEach(function (item) {
+    function itemAdded(item) {
         component.single(item.name, navMenu, null, item.created);
-    });
+    }
 
-    var menu = navMenuWidget.getNavMenu("edit-nav-menu-items");
-    menu.getItems().forEach(function (item) {
-        component.single(item.name, navMenu, null, item.created);
-    });
+    var menu = navmenu.getNavMenu("edit-nav-menu-items");
+    menu.getItems().forEach(itemAdded);
+    menu.itemAdded.add(null, itemAdded);
 });

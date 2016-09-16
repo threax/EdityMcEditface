1a113128a7271d584183d3c6adeb55bab56fb7ac
Andrew Piper <piper.andrew@spcollege.edu>
2016-09-16 15:17:43 -0400

    Added support for .net core back to Identity.NoSqlAuthorization.

--- Identity.NoSqlAuthorization/ClaimConverter.cs
+++ Identity.NoSqlAuthorization/ClaimConverter.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -12,7 +13,7 @@ namespace Identity.NoSqlAuthorization
     {
         public override bool CanConvert(Type objectType)
         {
-            return objectType.IsAssignableFrom(typeof(Claim));
+            return objectType.GetTypeInfo().IsAssignableFrom(typeof(Claim));
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)

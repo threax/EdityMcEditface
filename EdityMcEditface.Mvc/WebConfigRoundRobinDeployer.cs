92793454d34ec0f2c8d45476f70170aafb5b07a1
Andrew Piper <piper.andrew@spcollege.edu>
2017-02-16 15:37:11 -0500

    Properly escaped values, bumped version.

--- EdityMcEditface.Mvc/WebConfigRoundRobinDeployer.cs
+++ EdityMcEditface.Mvc/WebConfigRoundRobinDeployer.cs
@@ -41,7 +41,7 @@ namespace EdityMcEditface.Mvc
           <conditions logicalGrouping=""MatchAll"" >
             <add input=""{{REQUEST_FILENAME}}"" matchType=""IsFile"" ignoreCase=""false"" negate=""true"" />
             <add input=""{{REQUEST_FILENAME}}"" matchType=""IsDirectory"" ignoreCase=""false"" negate=""true"" />
-            <add input=""{REQUEST_URI}"" pattern=""^/(Service-)"" negate=""true"" />
+            <add input=""{{REQUEST_URI}}"" pattern=""^/(Service-)"" negate=""true"" />
           </conditions>
           <action type=""Rewrite"" url=""{0}/{{R:1}}"" />
         </rule>
@@ -50,7 +50,7 @@ namespace EdityMcEditface.Mvc
           <conditions logicalGrouping=""MatchAll"" >
             <add input=""{{REQUEST_FILENAME}}"" matchType=""IsFile"" negate=""true"" />
             <add input=""{{REQUEST_FILENAME}}"" matchType=""IsDirectory"" negate=""true"" />
-            <add input=""{REQUEST_URI}"" pattern=""^/(Service-)"" negate=""true"" />
+            <add input=""{{REQUEST_URI}}"" pattern=""^/(Service-)"" negate=""true"" />
           </conditions>
           <action type=""Rewrite"" url=""{{R:1}}.html"" />
         </rule>

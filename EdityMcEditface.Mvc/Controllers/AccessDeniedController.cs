610e9bd851503892cf71bcb5b775a44960c0ea05
Andrew Piper <piper.andrew@spcollege.edu>
2017-02-24 13:23:08 -0500

    Added a controller to show an access denied message.

---
+++ EdityMcEditface.Mvc/Controllers/AccessDeniedController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EdityMcEditface.Mvc.Controllers
+{
+    [Route("edity/[controller]")]
+    public class AccessDeniedController : Controller
+    {
+        public String Index()
+        {
+            return "Access Denied";
+        }
+    }
+}

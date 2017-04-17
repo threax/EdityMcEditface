63399678c0d119ac1de2c619956a6c391d85d87e
Andrew Piper <piper.andrew@spcollege.edu>
2017-04-17 09:43:40 -0400

    Updating to new build system.

---
+++ EdityMcEditface/build.js
@@ -0,0 +1,7 @@
+﻿var clientBuild = require('./Client/build');
+var tsc = require('threax-npm-tk/tsc');
+var jsonEditorBuild = require('htmlrapier.json-editor/build');
+
+jsonEditorBuild(__dirname + "/wwwroot/lib");
+clientBuild(__dirname + "/wwwroot/lib", __dirname + "/wwwroot");
+tsc();

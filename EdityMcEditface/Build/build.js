10f0c6c1374fbfff9d496c3750f9a97280138f5e
Andrew Piper <piper.andrew@spcollege.edu>
2017-04-20 09:40:44 -0400

    Moved site specific stuff out to the main folder.

--- EdityMcEditface/Build/build.js
+++ EdityMcEditface/Build/build.js
@@ -1,11 +1,30 @@
 "use strict";
 var edityCoreBuild = require("editymceditface.client/Build/build");
-var siteBuild = require("./../Site/Build/build");
+var clientBuild = require("editymceditface.client/Build/clientbuild");
+var copy = require('threax-npm-tk/copy');
+var less = require('threax-npm-tk/less');
 var tsc = require('threax-npm-tk/typescript');
 var filesDir = __dirname + "/..";
 edityCoreBuild.build(filesDir + "/ClientBin/EdityMcEditface", filesDir + "/wwwroot", filesDir + "/node_modules");
-siteBuild.build(filesDir + "/ClientBin/Site", filesDir + "/wwwroot", filesDir + "/node_modules");
+build(filesDir + "/ClientBin/Site", filesDir + "/wwwroot", filesDir + "/node_modules");
 tsc({
     projectFolder: filesDir
 });
+function build(outDir, iconOutPath, moduleDir) {
+    var promises = [];
+    //Build client
+    promises.push(clientBuild.build(outDir, iconOutPath, moduleDir, filesDir));
+    //Build bootstrap theme
+    promises.push(less.compile({
+        encoding: 'utf8',
+        importPaths: [moduleDir, moduleDir + '/bootstrap/less'],
+        input: filesDir + '/bootstrap/bootstrap-custom.less',
+        basePath: filesDir + '/bootstrap',
+        out: outDir + "/lib/bootstrap/dist/css",
+        compress: true,
+    }));
+    //Return composite promise
+    return Promise.all(promises);
+}
+exports.build = build;
 //# sourceMappingURL=build.js.map
--- EdityMcEditface/Build/build.ts
+++ EdityMcEditface/Build/build.ts
@@ -1,11 +1,34 @@
 ﻿import * as edityCoreBuild from 'editymceditface.client/Build/build';
-import * as siteBuild from './../Site/Build/build';
+import * as clientBuild from 'editymceditface.client/Build/clientbuild';
+
+var copy = require('threax-npm-tk/copy');
+var less = require('threax-npm-tk/less');
 var tsc = require('threax-npm-tk/typescript');
 
 var filesDir = __dirname + "/..";
 
 edityCoreBuild.build(filesDir + "/ClientBin/EdityMcEditface", filesDir + "/wwwroot", filesDir + "/node_modules");
-siteBuild.build(filesDir + "/ClientBin/Site", filesDir + "/wwwroot", filesDir + "/node_modules");
+build(filesDir + "/ClientBin/Site", filesDir + "/wwwroot", filesDir + "/node_modules");
 tsc({
     projectFolder: filesDir
 });
+
+export function build(outDir, iconOutPath, moduleDir): Promise<any> {
+    var promises = [];
+
+    //Build client
+    promises.push(clientBuild.build(outDir, iconOutPath, moduleDir, filesDir));
+
+    //Build bootstrap theme
+    promises.push(less.compile({
+        encoding: 'utf8',
+        importPaths: [moduleDir, moduleDir + '/bootstrap/less'],
+        input: filesDir + '/bootstrap/bootstrap-custom.less',
+        basePath: filesDir + '/bootstrap',
+        out: outDir + "/lib/bootstrap/dist/css",
+        compress: true,
+    }));
+
+    //Return composite promise
+    return Promise.all(promises);
+}

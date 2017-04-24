13b7692522fc77c84a21b821372b7e0ea5c53df5
Andrew Piper <piper.andrew@spcollege.edu>
2017-04-24 10:35:14 -0400

    Updated build and client side to latest versions. Tree shaking now setup.

--- EdityMcEditface/Build/build.ts
+++ EdityMcEditface/Build/build.ts
@@ -4,17 +4,16 @@ import * as clientBuild from 'editymceditface.client/Build/clientbuild';
 var copy = require('threax-npm-tk/copy');
 var less = require('threax-npm-tk/less');
 var tsc = require('threax-npm-tk/typescript').tsc;
+var jsnsTools = require('threax-npm-tk/jsnstools');
 
 var filesDir = __dirname + "/..";
 
-edityCoreBuild.build(filesDir + "/ClientBin/EdityMcEditface", filesDir + "/wwwroot", filesDir + "/node_modules");
 build(filesDir + "/ClientBin/Site", filesDir + "/wwwroot", filesDir + "/node_modules");
-tsc({
-    projectFolder: filesDir
-});
 
 export function build(outDir, iconOutPath, moduleDir): Promise<any> {
     var promises = [];
+
+    promises.push(edityCoreBuild.build(filesDir + "/ClientBin/EdityMcEditface", filesDir + "/wwwroot", filesDir + "/node_modules"));
 
     //Build client
     promises.push(clientBuild.build(outDir, iconOutPath, moduleDir, filesDir));
@@ -29,6 +28,16 @@ export function build(outDir, iconOutPath, moduleDir): Promise<any> {
         compress: true,
     }));
 
+    promises.push(compileTypescript());
+
     //Return composite promise
     return Promise.all(promises);
+}
+
+async function compileTypescript() {
+    await tsc({
+        projectFolder: filesDir
+    });
+
+    jsnsTools.saveLoadedModules(filesDir + '/ClientBin/Site/lib/tsbin.js', [], filesDir + '/ClientBin/Site/lib/tsbin.prod.js')
 }

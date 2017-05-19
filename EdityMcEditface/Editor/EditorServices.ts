f8e94648d74825403b2e9e491bc954c9d92e06e0
Andrew Piper <piper.andrew@spcollege.edu>
2017-05-19 09:42:39 -0400

    Updated to latest edity client.

--- EdityMcEditface/Editor/EditorServices.ts
+++ EdityMcEditface/Editor/EditorServices.ts
@@ -1,27 +1,33 @@
 ﻿import * as bootstrap from 'hr.bootstrap.all';
-import { AccessTokenManager } from 'hr.accesstokens';
 import { Fetcher } from 'hr.fetcher';
 import { CacheBuster } from 'hr.cachebuster';
 import { WindowFetch } from 'hr.windowfetch';
 import { WithCredentialsFetcher } from 'edity.editorcore.WithCredentialsFetcher';
-import { CompilerService } from 'edity.editorcore.CompileService';
-import * as di from 'hr.di';
 import * as urlInjector from 'edity.editorcore.BaseUrlInjector';
+import * as controller from 'hr.controller';
 
 interface PageSettings {
     baseUrl;
 }
 
-export function addServices(services: di.ServiceCollection) {
-    bootstrap.activate();
+var mainBuilder: controller.InjectedControllerBuilder = null;
 
-    var pageSettings = <PageSettings>(<any>window).editPageSettings;
-    if (!pageSettings) {
-        pageSettings = {
-            baseUrl: '/'
-        };
+export function createBaseBuilder(): controller.InjectedControllerBuilder {
+    if (!mainBuilder) {
+        mainBuilder = new controller.InjectedControllerBuilder();
+
+        bootstrap.activate();
+
+        var pageSettings = <PageSettings>(<any>window).editPageSettings;
+        if (!pageSettings) {
+            pageSettings = {
+                baseUrl: '/'
+            };
+        }
+
+        var services = mainBuilder.Services;
+        services.tryAddShared(Fetcher, s => new WithCredentialsFetcher(new CacheBuster(new WindowFetch())));
+        services.tryAddShared(urlInjector.IBaseUrlInjector, s => new urlInjector.BaseUrlInjector(pageSettings.baseUrl));
     }
-
-    services.tryAddShared(Fetcher, s => new WithCredentialsFetcher(new CacheBuster(new WindowFetch())));
-    services.tryAddShared(urlInjector.IBaseUrlInjector, s => new urlInjector.BaseUrlInjector(pageSettings.baseUrl));
+    return mainBuilder;
 }

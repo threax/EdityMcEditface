6a980f8e155e1fdf931ec0cc6017a24beefdff1d
Andrew Piper <piper.andrew@spcollege.edu>
2017-10-26 09:22:30 -0400

    Fixed bug where enumerable over content files was not reentrant.

--- EdityMcEditface.HtmlRenderer/Filesystem/FileFinder.cs
+++ EdityMcEditface.HtmlRenderer/Filesystem/FileFinder.cs
@@ -423,21 +423,7 @@ namespace EdityMcEditface.HtmlRenderer.Filesystem
         public IEnumerable<String> EnumerateContentFiles(String path, String searchPattern = "*", SearchOption searchOption = SearchOption.TopDirectoryOnly)
         {
             var seenPaths = new HashSet<String>();
-            return EnumerateContentFiles(seenPaths, path, searchPattern, searchOption)
-                .Concat(ResetHashes<String>(seenPaths));
-        }
-
-        /// <summary>
-        /// This funciton looks like an enumerable, but really we concat it with the end of EnumerateContentFiles or EnumerateContentDirectories to reset the hashset
-        /// that stores the visited file names.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="hashes"></param>
-        /// <returns></returns>
-        private IEnumerable<T> ResetHashes<T>(HashSet<T> hashes)
-        {
-            hashes.Clear();
-            yield break;
+            return EnumerateContentFiles(seenPaths, path, searchPattern, searchOption).ToList(); //Have to use tolist or else this enumerable is not reentrant
         }
 
         private IEnumerable<String> EnumerateContentFiles(HashSet<String> seenPaths, String path, String searchPattern, SearchOption searchOption)
@@ -474,8 +460,7 @@ namespace EdityMcEditface.HtmlRenderer.Filesystem
         public IEnumerable<String> EnumerateContentDirectories(String path, String searchPattern = "*", SearchOption searchOption = SearchOption.TopDirectoryOnly)
         {
             var seenPaths = new HashSet<String>();
-            return EnumerateContentDirectories(seenPaths, path, searchPattern, searchOption)
-                .Concat(ResetHashes<String>(seenPaths));
+            return EnumerateContentDirectories(seenPaths, path, searchPattern, searchOption).ToList(); //Have to use tolist or else this enumerable is not reentrant
         }
 
         public IEnumerable<String> EnumerateContentDirectories(HashSet<String> seenPaths, String path, String searchPattern, SearchOption searchOption)

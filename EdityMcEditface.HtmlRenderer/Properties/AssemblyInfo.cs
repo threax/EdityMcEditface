a56a37c06f777cd94d8fac5da730074dcd4af7f3
Andrew Piper <piper.andrew@spcollege.edu>
2018-01-09 10:50:30 -0500

    Added nuget info, changed license to apache.

--- EdityMcEditface.HtmlRenderer/Properties/AssemblyInfo.cs
+++ EdityMcEditface.HtmlRenderer/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EdityMcEditface.HtmlRenderer")]
+[assembly: AssemblyProduct("Edity McEditface")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
--- EdityMcEditface.Mvc/Properties/AssemblyInfo.cs
+++ EdityMcEditface.Mvc/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EdityMcEditface.Mvc")]
+[assembly: AssemblyProduct("Edity McEditface")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible

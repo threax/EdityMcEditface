323fde21df049d773413892e0535453944df8381
Andrew Piper <andrew@threax.com>
2021-01-20 09:38:33 -0500

    Added push script.

---
+++ Push.ps1
@@ -0,0 +1,4 @@
+git push
+git push https://github.com/threax/EdityMcEditface.git
+git push https://gitlab.com/threax-software/editymceditface.git
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/EdityMcEditface
